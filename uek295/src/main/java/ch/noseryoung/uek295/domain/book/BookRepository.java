3c6c4a786af9109c3e4a41fa6db0fcac345968a4
LorenaVennemann <113357105+LorenaVennemann@users.noreply.github.com>
2023-04-14 12:06:55 +0200

    New package domain.Entry created and all classes for Entry created

--- uek295/src/main/java/ch/noseryoung/uek295/domain/book/BookRepository.java
+++ uek295/src/main/java/ch/noseryoung/uek295/domain/book/BookRepository.java
@@ -1,4 +1,7 @@
 package ch.noseryoung.uek295.domain.book;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer>{
+
 }
---
+++ uek295/src/main/java/ch/noseryoung/uek295/domain/entry/BlogEntryController.java
@@ -0,0 +1,4 @@
+package ch.noseryoung.uek295.domain.entry;
+
+public class BlogEntryController {
+}
